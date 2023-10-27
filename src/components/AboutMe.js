755c6627de6952a0beeb789be5f15b74efb30744
Bb0lt <basil.kanaan@mail.utoronto.ca>
2023-10-27 19:21:44 -0400

    improved copywriting

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -29,100 +29,96 @@ const AboutMe = () => {
     };
 
     return (
-        <Container
-            sx={{
-                minHeight: '100vh',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-            }}
-        >
-            <Box sx={{textAlign: 'center', p: "4rem"}}>
-                <motion.div
-                    variants={paragraphVariants}
-                    initial="hidden"
-                    animate={scrollY > window.innerHeight / 1.5 - 200 ? "visible" : "hidden"}
-                    transition={{duration: 1}}
+        <div style={{background: 'linear-gradient(to bottom, rgba(0,0,0,1) 0%, rgba(0, 0, 0, 0) 50%)'}}>
+            <Container
+                sx={{
+                    p: 5,
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+            >
+                <Box sx={{
+                    textAlign: 'center',
+                    p: "4rem",
+                }}
                 >
-                    <Typography variant="h1" sx={{
-                        mt: 4,
-                        mb: 2,
-                        fontSize: "4rem",
-                        fontWeight: "bold",
-                        textShadow: '2px 2px 10px rgba(0, 0, 0, 0.9)'
-                    }}>
-                        About Me
-                    </Typography>
-
-                    <div>
-                        <Typography variant="h2" sx={{
-                            color: "#4DA1FF",
-                            fontWeight: 'bold',
-                            fontSize: '2rem',
-                            lineHeight: 1.6,
-                            textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)'
-                        }}>
-                            <i>Here to serve and inspire.</i>
-                            <br/><br/>
+                    <motion.div
+                        variants={paragraphVariants}
+                        initial="hidden"
+                        animate={scrollY > window.innerHeight / 2 ? "visible" : "hidden"}
+                        transition={{duration: 1}}
+                    >
+                        <Typography variant={"h2"}>
+                            About Me
                         </Typography>
-                    </div>
 
-                    <motion.div
-                        variants={paragraphVariants}
-                        initial="hiddenU"
-                        animate={scrollY > window.innerHeight / 1.5 ? "visible" : "hiddenU"}
-                        transition={{duration: 1}}
-                        style={{textAlign: 'center'}}
-                    >
-                        <div>
-                            Hi! <br/>
-                            I'm a software engineer.
-                            <br/> I make websites.
-                        </div>
-                    </motion.div>
+                        <Typography variant={"h3"}>
+                            Here to serve and inspire.
+                        </Typography>
 
-                    <br/><br/>
+                        <br/><br/>
+
+                        <motion.div
+                            variants={paragraphVariants}
+                            initial="hiddenL"
+                            animate={scrollY > window.innerHeight / 2 + 635 / 4 ? "visible" : "hiddenL"}
+                            transition={{duration: 1}}
+                            style={{textAlign: 'left'}}
+                        >
+                            <Typography variant={"body3"}>
+                                Hi, I'm a software engineer and I make websites.
+                            </Typography>
+                        </motion.div>
+
+                        <br/><br/>
+                        <motion.div
+                            variants={paragraphVariants}
+                            initial="hiddenL"
+                            animate={scrollY > window.innerHeight / 2 + 635 / 4 * 2 ? "visible" : "hiddenL"}
+                            transition={{duration: 1}}
+                            style={{textAlign: 'left'}}
+                        >
+                            <Typography variant={"body3"}>
+                                I'm here to make your life easier and turn your ideas into reality. Let's simplify the
+                                process and achieve your visions.
+                            </Typography>
+                        </motion.div>
+                        <br/><br/>
+                        <motion.div
+                            variants={paragraphVariants}
+                            initial="hiddenL"
+                            animate={scrollY > window.innerHeight / 2 + 635 / 4 * 3 ? "visible" : "hiddenL"}
+                            transition={{duration: 1}}
+                            style={{textAlign: 'left'}}
+                        >
+                            <Typography variant={"body3"}>
+                                Beyond code, I'm your trusted partner. You can count on me to deliver your projects with
+                                dedication and integrity.
+                                Saying "yes" to your digital aspirations has never been so easy.
+                            </Typography>
+                        </motion.div>
+                    </motion.div>
+                    <br/>
                     <motion.div
                         variants={paragraphVariants}
-                        initial="hiddenL"
-                        animate={scrollY > window.innerHeight / 1.5 + 200 ? "visible" : "hiddenL"}
-                        transition={{duration: 1}}
-                        style={{textAlign: 'center'}}
+                        initial="hiddenD"
+                        animate={scrollY > window.innerHeight / 2 + 635 ? "visible" : "hiddenD"}
+                        transition={{duration: 0.5}}
+
                     >
-                        <div>
-                            My journey is fueled by a relentless drive to deliver excellence. I believe in the
-                            power of technology to transform ideas into reality, and I take pride in building reliable
-                            and robust software that stands the test of time.
+                        <div style={{color: "lightblue"}}>
+                            Let's get started!
                         </div>
+                        <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                            <ThumbUpAltIcon sx={{fontSize: '5rem', marginRight: '0.5rem', color: '#4DA1FF'}}/>
+                            <CheckCircleIcon sx={{fontSize: '5rem', color: '#4DA1FF'}}/>
+                        </Box>
                     </motion.div>
-                    <br/><br/>
-                    <motion.div
-                        variants={paragraphVariants}
-                        initial="hiddenR"
-                        animate={scrollY > window.innerHeight / 1.5 + 400 ? "visible" : "hiddenR"}
-                        transition={{duration: 1}}
-                    >
-                        <div>
-                            Beyond code, I am driven by a commitment to trustworthiness and dependability.
-                            Clients and collaborators can rely on me to bring their visions to life with unwavering
-                            dedication and integrity. Let's work together to turn possibilities into reality!
-                        </div>
-                    </motion.div>
-                </motion.div>
-                <br/><br/>
-                <motion.div
-                    variants={paragraphVariants}
-                    initial="hiddenD"
-                    animate={scrollY > window.innerHeight / 1.5 + 600 ? "visible" : "hiddenD"}
-                    transition={{duration: 0.5}}
-                >
-                    <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-                        <ThumbUpAltIcon sx={{fontSize: '5rem', marginRight: '0.5rem', color: '#4DA1FF'}}/>
-                        <CheckCircleIcon sx={{fontSize: '5rem', color: '#4DA1FF'}}/>
-                    </Box>
-                </motion.div>
-            </Box>
-        </Container>
+                </Box>
+            </Container>
+        </div>
+
     );
 };
 
