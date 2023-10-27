9f1497a612bacf7c80e4caf743f1b0d197efcee0
Bb0lt <basil.kanaan@mail.utoronto.ca>
2023-10-27 19:23:46 -0400

    removed custom dividers

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,19 +2,29 @@ import React from 'react';
 import HeroSection from '../components/HeroSection';
 import ServicesSection from '../components/ServicesSection';
 import ContactSection from "../components/ContactSection";
-import SectionDivider from "../components/SectionDivider";
+// import SectionDivider from "../components/SectionDivider";
 import AboutMe from "../components/AboutMe";
+import ProjectsSection from "../components/ProjectsSection";
 
 const Home = () => {
 
     return (
         <div>
-            <HeroSection/>
-            <AboutMe/>
-            <SectionDivider/>
-            <ServicesSection/>
-            <SectionDivider/>
-            <ContactSection/>
+            <div id="hero-section">
+                <HeroSection/>
+            </div>
+            <div id="about-section">
+                <AboutMe/>
+            </div>
+            <div id="services-section">
+                <ServicesSection/>
+            </div>
+            <div id="projects-section">
+                <ProjectsSection/>
+            </div>
+            <div id="contact-section">
+                <ContactSection/>
+            </div>
         </div>
     );
 };
